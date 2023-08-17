72ce71065fb591867c6e08ac3c6aa695e2b9948a
shrapp <shahar.rapp@live.biu.ac.il>
2023-08-17 14:56:08 +0300

    fix

--- calc_data.py
+++ calc_data.py
@@ -1,12 +1,8 @@
-import numpy as np
-
 from functions import calc_data
+import numpy as np
 
 Ns = [50,100]
 
-# taus = np.logspace(-5, 0, 25)
-# ws = [0.0, 0.1, 0.05, 0.2, 0.5, 1.0, 5.0, 0.01]
-
-# calc_data(Ns, taus, ws)
-
-calc_data(Ns, [0.12345], [0.12345])
+taus = np.logspace(-5, 0, 25)
+ws = [0.0, 0.1, 0.05, 0.2, 0.5, 1.0, 5.0, 0.01]
+calc_data(Ns, taus, ws)
