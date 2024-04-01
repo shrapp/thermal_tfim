cd3932d1836c16d7741077c7cb4f32d09b7b451f
shrapp <shahar.rapp@live.biu.ac.il>
2024-04-01 14:42:37 +0300

    Back to work changes

--- functions.py
+++ functions.py
@@ -10,37 +10,83 @@ from scipy.special import gammaln, logsumexp
 
 logging.basicConfig(filename='logger.log', level=logging.INFO, format='%(asctime)s %(message)s')
 
+def commutator(A, B):
+    """
+    Calculate the commutator of two matrices A and B.
+
+    Parameters:
+    A (np.ndarray): A square matrix.
+    B (np.ndarray): Another square matrix of the same dimension as A.
+
+    Returns:
+    np.ndarray: The commutator [A, B] = AB - BA.
+    """
+    return np.dot(A, B) - np.dot(B, A)
+
+def dag(matrix):
+    """
+    Calculate the conjugate transpose (dagger) of a complex matrix.
+
+    Parameters:
+    matrix (np.ndarray): A complex matrix.
+
+    Returns:
+    np.ndarray: The conjugate transpose of the matrix.
+    """
+    return np.conjugate(matrix).T
+
 # Parameters
 hbar = 1.0   # in natural units
 epsilon = 1e-8
 data_file = 'data.csv'
+sigX = np.array([[0, 1], [1, 0]])
+sigZ = np.array([[1, 0], [0, -1]])
 
-def g(t, tau, t_max):
-    return (t_max-t)/tau if tau > 0 else t_max
+def g0(t, tau):
+    return t/tau if tau > 0 else 0
 
-def H(t, tau, t_max, k):
-    g_f = g(t, tau, t_max)
-    return np.array([[float(g_f) - np.cos(k), np.sin(k)], [np.sin(k), -(g_f - np.cos(k))]])
+def H0(t, tau, k):
+    g_f = g0(t, tau)
+    h_z = 2 * (1 - g_f - g_f * np.cos(k))
+    h_x = 2 * g_f * np.sin(k)
+    H0 = h_z * sigZ + h_x * sigX
+    return H0
 
-def psi_dt(t, psi,  tau, t_max, k):
-    return -1j * np.dot(H(t, tau, t_max, k), psi)
+def V_func(k):
+    v_x = 2 * np.sin(k)
+    v_z = -2 * (np.cos(k) + 1)
+    V_matrix = v_z * sigZ + v_x * sigX
+    return V_matrix
 
-def rho_dt(t, rho, tau, t_max, k, w, noise_matrix):
-    h = H(t, tau, t_max, k)
-    # convert rho from vector to matrix
-    rho = rho.reshape((2,2))
-    D = np.dot(noise_matrix, np.dot(rho,noise_matrix.H)) - 0.5 * (np.dot(rho, np.dot(noise_matrix.H,noise_matrix)) + np.dot(noise_matrix.H, np.dot(noise_matrix,rho)))
-    return (-1j * (np.dot(h, rho) - np.dot(rho, h)) + w**2 * D).flatten()
+def Dissipator(k, rho, w):
+    V = V_func(k)
+    V_dag = dag(V)
+    term1 = V * rho * V_dag
+    term2 = 0.5 * V_dag * V * rho
+    term3 = 0.5 * rho * V_dag * V
+    D = w**2 * (term1 - term2 - term3)
+    return D
+
+def psi_dt(t, psi,  tau, k):
+    return -1j * np.dot(H0(t, tau, k), psi)
+
+def rho_dt(t, rho, tau, k, w):
+    rho = rho.reshape(2,2)
+    H = H0(t, tau, k)
+    D = Dissipator(k, rho, w)
+    U = -1j * commutator(H, rho)
+    rho_dot = U + D
+    return rho_dot.flatten()
 
 def lz_time_evolution_single_k(k, t_max, tau):
     # use scipy.integrate.solve_ivp to solve the ODE H(t) psi(t) = i hbar d/dt psi(t)
     psi0 = np.array([0+0j, 1+0j])
     return  solve_ivp(fun=psi_dt, method='DOP853', t_span=(0, t_max), y0=psi0, args=(tau, t_max, k), rtol = epsilon, atol = epsilon)
 
-def noisy_lz_time_evolution_single_k(k, t_max, tau, w, noise_matrix):
+def noisy_lz_time_evolution_single_k(k, tau, w):
     # use scipy.integrate.solve_ivp to solve the ODE H(t) psi(t) = i hbar d/dt psi(t)
     rho0 = np.array([[0+0j, 0+0j], [0+0j, 1+0j]])
-    return  solve_ivp(fun=rho_dt, method='DOP853', t_span=(0, t_max), y0=rho0.flatten(), args=(tau, t_max, k, w, noise_matrix), rtol = epsilon, atol = epsilon)
+    return  solve_ivp(fun=rho_dt, method='DOP853', t_span=(0, tau), y0= rho0.flatten(), args=(tau, k, w), rtol = epsilon, atol = epsilon, vectorized=True)
 
 def lz_time_evolution(ks, tau):
     t_max = 100*tau
@@ -49,11 +95,10 @@ def lz_time_evolution(ks, tau):
         results = pool.starmap(lz_time_evolution_single_k, [(k, t_max, tau) for k in ks])
     return results
 
-def noisy_lz_time_evolution(ks, tau, w, noise_matrix):
-    t_max = 100*tau
+def noisy_lz_time_evolution(ks, tau, w):
     # use pool to parallelize the calculation for each k
     with Pool() as pool:
-        results = pool.starmap(noisy_lz_time_evolution_single_k, [(k, t_max, tau, w, noise_matrix) for k in ks])
+        results = pool.starmap(noisy_lz_time_evolution_single_k, [(k, tau, w) for k in ks])
     return results
 
 
@@ -65,9 +110,9 @@ def calc_pk(ks, tau):
     # remove valuse that are too small
     return  np.where(pks < epsilon/100, 0, pks)
 
-def calk_noisy_pk(ks, tau, w, noise_matrix):
+def calk_noisy_pk(ks, tau, w):
     # calculate the time evolution for each k
-    results = noisy_lz_time_evolution(ks, tau, w, noise_matrix)
+    results = noisy_lz_time_evolution(ks, tau, w)
     # calculate the probabilities for each k
     pks = np.array([np.abs(np.dot(np.array([np.sin(k/2), np.cos(k/2)]), np.dot(result.y[:,-1].reshape(2,2), np.array([[np.sin(k/2)], [np.cos(k/2)]]))))[0] for k ,result in zip(ks, results)])
     # remove valuse that are too small
@@ -75,11 +120,8 @@ def calk_noisy_pk(ks, tau, w, noise_matrix):
      
 
 def k_f(N):
-    # Generating an array from -N to N
-    full_array = np.arange(-N+1, N)
-    
     # Filtering out even numbers
-    odd_numbers = full_array[full_array % 2 != 0]
+    odd_numbers = np.arange(1, N, 2)
     
     # Multiply each odd number by pi/N
     return odd_numbers * np.pi / N
@@ -88,7 +130,7 @@ def p_k_analytic(tau, k):
     return np.exp(-2*np.pi*tau*(k**2))*(np.cos(k/2)**2)
 
 def ln_P_tilda_func(theta, pks):
-    return np.sum(np.log(1 + pks * (np.exp(1j * theta) - 1)))
+    return np.sum(np.log(1 + pks * (np.exp(2 * 1j * theta) - 1)))
 
 def integrand_func(theta, pks, d):
     return np.exp(ln_P_tilda_func(theta, pks) - 1j * theta * d)
